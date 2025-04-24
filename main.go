f02d383ad17ccb180814c25d9f6e04a6faedbcba
Artem Zakirullin <artemzr@gmail.com>
2025-04-24 19:48:19 +0300

    fix hyperparams

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ const (
 	heads            = 4
 	layers           = 4
 	epochs           = 20000
-	learningRate     = 0.0005
+	learningRate     = 0.001
 	evalIters        = 1000
 	dropout          = 0.0  // disable some % of our neurons to prevent overfitting, model is likely to generalize
 	lossScale        = 1.0  // we don't use batches, so scaling loss down may help better convergence
