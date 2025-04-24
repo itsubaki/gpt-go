d2b9115d97379fe9122e44c22b2e72ac9ff26c08
Artem Zakirullin <artemzr@gmail.com>
2025-04-24 14:09:54 +0300

    fix merge linear & norm to layer

--- layer.go
+++ layer.go
@@ -1,11 +1,22 @@
-// Add embeddings
 package main
 
 import (
 	"math"
 
+	"github.com/itsubaki/autograd/layer"
 	"github.com/itsubaki/autograd/matrix"
 	"github.com/itsubaki/autograd/variable"
+
+	"gptgo/pkg"
+)
+
+var (
+	Mean        = pkg.Mean
+	Variance    = pkg.Variance
+	Div         = pkg.Div
+	Mul         = variable.Mul
+	Pow         = variable.Pow
+	RandWeights = xavier
 )
 
 type Linear struct {
@@ -16,11 +27,8 @@ type Linear struct {
 	BiasGrad *variable.Variable
 }
 
-var RandWeights = xavier
-
 type LinearOption func(*Linear)
 
-// TODO rename from new Linear to something other?
 func NewLinear(in, out int, opts ...LinearOption) *Linear {
 	l := &Linear{
 		In:     in,
@@ -55,6 +63,38 @@ func (l *Linear) Forward(input *variable.Variable) *variable.Variable {
 	}
 
 	return logits
+}
+
+type LayerNorm struct {
+	Scale *variable.Variable
+	Shift *variable.Variable
+	eps   float64
+}
+
+func NewLayerNorm(dim int) *LayerNorm {
+	return &LayerNorm{
+		eps:   1e-05,
+		Scale: Ones(1, dim),
+		Shift: Zeros(1, dim),
+	}
+}
+
+// It is implemented using existing primitives, so backprop will work
+func (ln *LayerNorm) Forward(x *variable.Variable) *variable.Variable {
+	xmean := Mean(x)
+	xvar := Variance(x)
+	eps := variable.New(ln.eps)
+	xhat := Div(Sub(x, xmean), Pow(0.5)(Add(xvar, eps)))
+	out := Add(Mul(ln.Scale, xhat), ln.Shift)
+
+	return out
+}
+
+func (ln *LayerNorm) Params() []layer.Parameter {
+	return []layer.Parameter{
+		ln.Scale,
+		ln.Shift,
+	}
 }
 
 func xavier(inSize, outSize int) *variable.Variable {
